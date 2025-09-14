9e1e4aaaf18622425e74cb50c6ee620caae74af8
as3n1n <lumnadiscord@gmail.com>
2025-09-15 00:16:43 +0200

    Delete hebi directory

--- hebi/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-secondary text-text py-6 text-center">
-      <p>© {new Date().getFullYear()} Javelin. All rights reserved.</p>
-    </footer>
-  );
-}
