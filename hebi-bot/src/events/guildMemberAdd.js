7f02e0d4e7e113d6e3a4cf38fa827a411a90fcb7
as3n1n <lumnadiscord@gmail.com>
2025-09-15 01:11:41 +0200

    Update guildMemberAdd.js

--- hebi-bot/src/events/guildMemberAdd.js
+++ hebi-bot/src/events/guildMemberAdd.js
@@ -2,11 +2,14 @@ module.exports = (client) => {
   client.on("guildMemberAdd", async (member) => {
     try {
       const link = `https://javelin.asia/verify?uid=${member.id}`;
-      await member.send(
-        `Welcome **${member.user.username}**!\n\nPlease verify your account here: ${link}`
-      );
+
+      await member.send({
+        content: `Welcome **${member.user.username}**!\n\nPlease verify your account here to get access: ${link}`,
+      });
+
+      console.log(`Verification link sent to ${member.user.tag}`);
     } catch (err) {
-      console.error("Failed to send DM:", err);
+      console.error(`Failed to send DM to ${member.user.tag}:`, err.message);
     }
   });
 };
