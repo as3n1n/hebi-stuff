234e97b209ee4934244d313612a364d12b990bca
as3n1n <lumnadiscord@gmail.com>
2025-09-15 03:00:58 +0200

    Update App.jsx

--- hebi-site/src/pages/App.jsx
+++ hebi-site/src/pages/App.jsx
@@ -1,21 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar.jsx";
-import Footer from "../components/Footer.jsx";
-import Hero from "../components/Hero.jsx";
-import Features from "../components/Features.jsx";
-import Verify from "./Verify.jsx";
-import Invite from "./Invite.jsx";
-import Login from "./Login.jsx";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Login from "./Login";
+import Verify from "./Verify";
+import Invite from "./Invite";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-black text-white font-[MuseoSansCyrl-900]">
+      <div className="flex flex-col min-h-screen bg-black">
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<><Hero /><Features /></>} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/verify" element={<Verify />} />
             <Route path="/invite" element={<Invite />} />
           </Routes>
