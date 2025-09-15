f7e426d1fe61fa83c73530b7d0f7f8aa30aeab29
as3n1n <lumnadiscord@gmail.com>
2025-09-15 03:21:56 +0200

    Update App.jsx

--- hebi-site/src/pages/App.jsx
+++ hebi-site/src/pages/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Home from "./Home";
-import Verify from "./Verify";
-import Invite from "./Invite";
-import About from "./About";
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import Home from "./Home.jsx";
+import Verify from "./Verify.jsx";
+import Invite from "./Invite.jsx";
+import About from "./About.jsx";
 
 export default function App() {
   return (
