3d957fde40f3fa7722bcda2b39028b15207a7d46
as3n1n <lumnadiscord@gmail.com>
2025-09-15 23:51:32 +0200

    Update BackgroundAnimation.jsx

--- hebi-site/src/components/BackgroundAnimation.jsx
+++ hebi-site/src/components/BackgroundAnimation.jsx
@@ -1,35 +1,15 @@
-import React from "react";
-
 export default function BackgroundAnimation() {
   return (
-    <div className="absolute inset-0 overflow-hidden -z-10">
-      <svg
+    <div className="absolute inset-0 -z-10">
+      <div
         className="w-full h-full"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <linearGradient id="grad" x1="0" x2="1" y1="0" y2="1">
-            <stop offset="0%" stopColor="#8B0000" stopOpacity="0.3" />
-            <stop offset="100%" stopColor="#000000" stopOpacity="0.7" />
-          </linearGradient>
-        </defs>
-        <path
-          d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z"
-          fill="url(#grad)"
-        >
-          <animate
-            attributeName="d"
-            dur="15s"
-            repeatCount="indefinite"
-            values="
-              M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z;
-              M0,120 C180,0 320,220 500,120 L500,00 L0,0 Z;
-              M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z
-            "
-          />
-        </path>
-      </svg>
+        style={{
+          background: "linear-gradient(to bottom, #1a0000, #000000)",
+          maskImage: "url('/topography.svg')",
+          WebkitMaskImage: "url('/topography.svg')",
+          opacity: 0.3,
+        }}
+      />
     </div>
   );
 }
