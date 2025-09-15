26689330d7eff43e876d388a3305866a8d8c967f
as3n1n <lumnadiscord@gmail.com>
2025-09-16 00:36:34 +0200

    Update vite.config.js

--- hebi-site/vite.config.js
+++ hebi-site/vite.config.js
@@ -1,16 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects", //
+          dest: ".",                // 
+        },
+      ],
+    }),
+  ],
   base: "/",
-  publicDir: "public", // assure que _redirects est copié
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    open: true,
   },
 });
