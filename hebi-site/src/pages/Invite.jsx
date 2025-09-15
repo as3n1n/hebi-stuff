b11bbd00d880723d4df15712102a5cf1f3bca856
as3n1n <lumnadiscord@gmail.com>
2025-09-15 03:13:13 +0200

    Update Invite.jsx

--- hebi-site/src/pages/Invite.jsx
+++ hebi-site/src/pages/Invite.jsx
@@ -1,10 +1,16 @@
 export default function Invite() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <h1 className="text-4xl font-bold text-red-600 mb-6">Ajouter Hebi à ton serveur</h1>
+      <h1 className="text-4xl text-red-600 font-bold mb-6">Inviter Hebi</h1>
+      <p className="text-gray-300 max-w-xl mb-8">
+        Ajoutez Hebi à votre serveur Discord pour protéger votre communauté
+        contre les faux comptes et améliorer l’expérience de vos membres.
+      </p>
       <a
         href="https://discord.com/oauth2/authorize?client_id=1327772341639581747"
-        className="px-6 py-3 bg-red-600 hover:bg-red-700 text-white font-bold rounded-xl transition-all"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-6 py-3 bg-red-600 rounded-lg font-bold hover:bg-red-700 transition-all"
       >
         Inviter Hebi
       </a>
