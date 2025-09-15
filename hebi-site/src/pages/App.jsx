f2b9f2ec1bfc8d55ce801c755867a69ed12e7855
as3n1n <lumnadiscord@gmail.com>
2025-09-15 02:46:52 +0200

    Update App.jsx

--- hebi-site/src/pages/App.jsx
+++ hebi-site/src/pages/App.jsx
@@ -1,19 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout";
-import Login from "./Login";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
 import Verify from "./Verify";
 import Invite from "./Invite";
+import Login from "./Login";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/verify" element={<Verify />} />
-          <Route path="/invite" element={<Invite />} /> {/* ✅ nouvelle page */}
-        </Routes>
-      </Layout>
+      <div className="flex flex-col min-h-screen bg-black text-white font-[MuseoSansCyrl-900]">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Contenu */}
+        <main className="flex-grow">
+          <Routes>
+            {/* Page d’accueil */}
+            <Route path="/" element={<><Hero /><Features /></>} />
+
+            {/* Page de login */}
+            <Route path="/login" element={<Login />} />
+
+            {/* Vérification (captcha) */}
+            <Route path="/verify" element={<Verify />} />
+
+            {/* Invitation du bot */}
+            <Route path="/invite" element={<Invite />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
