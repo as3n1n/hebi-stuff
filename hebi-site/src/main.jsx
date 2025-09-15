127a5301aec7632685d99d6be11e6191c58f44ca
as3n1n <lumnadiscord@gmail.com>
2025-09-15 02:21:03 +0200

    Update main.jsx

--- hebi-site/src/main.jsx
+++ hebi-site/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App"; //
-import "./styles/globals.css"; //
+import App from "./styles/App.jsx";   // <-- chemin corrigé
+import "./styles/globals.css";       // <-- ton css global
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
