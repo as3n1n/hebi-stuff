d1586dad35d6936af885b02fbf185eef1a8653b9
as3n1n <lumnadiscord@gmail.com>
2025-09-17 17:18:14 +0200

    Update Home.jsx

--- hebi-site/src/pages/Home.jsx
+++ hebi-site/src/pages/Home.jsx
@@ -3,7 +3,17 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="bg-black text-red-600 min-h-screen flex flex-col justify-center items-center">
-      <img src="/logo.png" alt="Hebi" className="w-40 mb-8" />
+      {/* Logo cliquable */}
+      <a
+        href="https://javelin.asia"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="transition-transform hover:scale-110"
+      >
+        <img src="/logo.png" alt="Hebi" className="w-40 mb-8" />
+      </a>
+
+      {/* Navigation */}
       <div className="space-x-6 text-xl">
         <Link to="/upload" className="hover:underline">[upload]</Link>
         <Link to="/docs" className="hover:underline">[docs]</Link>
