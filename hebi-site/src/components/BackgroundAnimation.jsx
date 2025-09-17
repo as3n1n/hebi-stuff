e594b4d46f1f3095182af9cc7e33d764bde39bd2
as3n1n <lumnadiscord@gmail.com>
2025-09-17 03:38:11 +0200

    Delete hebi-site directory

--- hebi-site/src/components/BackgroundAnimation.jsx
+++
@@ -1,7 +0,0 @@
-export default function BackgroundAnimation() {
-  return (
-    <div className="absolute inset-0 z-0">
-      <div className="w-full h-full bg-gradient-to-br from-black via-hebiBlack to-red-900 animate-pulse"></div>
-    </div>
-  );
-}
