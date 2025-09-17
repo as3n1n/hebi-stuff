be5f943f307d1b6de2acb73bb20904bec0316c6b
as3n1n <lumnadiscord@gmail.com>
2025-09-17 05:44:55 +0200

    Update server.js

--- hebi-api/server.js
+++ hebi-api/server.js
@@ -94,4 +94,4 @@ app.get("/f/:filename", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Hebi API running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Hebi API running on port ${PORT}`));
