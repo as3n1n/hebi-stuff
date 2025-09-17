607aff72b304a9a72995e7c1f6e6c642b859df28
as3n1n <lumnadiscord@gmail.com>
2025-09-17 02:26:03 +0200

    Update App.jsx

--- hebi-site/src/App.jsx
+++ hebi-site/src/App.jsx
@@ -3,23 +3,38 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import BackgroundAnimation from "./components/BackgroundAnimation";
 import Home from "./pages/Home";
-import Status from "./pages/Status";
-import AddBot from "./pages/AddBot";
-import Login from "./pages/Login";
-import Panel from "./pages/Panel";
+import Upload from "./pages/Upload";
+import Faq from "./pages/Faq";
+import ImportSave from "./pages/ImportSave";
+import Banned from "./pages/Banned";
+import KeyGuard from "./components/KeyGuard";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col relative overflow-hidden">
+    <div className="min-h-screen flex flex-col relative overflow-hidden bg-black text-white">
       <BackgroundAnimation />
       <Navbar />
       <main className="flex-grow relative z-10">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/status" element={<Status />} />
-          <Route path="/addbot" element={<AddBot />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/panel" element={<Panel />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route
+            path="/upload"
+            element={
+              <KeyGuard>
+                <Upload />
+              </KeyGuard>
+            }
+          />
+          <Route
+            path="/import"
+            element={
+              <KeyGuard>
+                <ImportSave />
+              </KeyGuard>
+            }
+          />
+          <Route path="/banned" element={<Banned />} />
         </Routes>
       </main>
       <Footer />
